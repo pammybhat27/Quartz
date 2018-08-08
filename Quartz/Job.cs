f8cd8888b48b3d7dc24ad92f190cfd4d75feba2a
Paramanand Bhat <paramanand@talen.com.au>
2018-08-08 16:31:39 +1000

    Simple quartz project
    Windows s ervice created using quartz.net plugin

---
+++ Quartz/Job.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quartz
+{
+    public class TestJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            // Send Email Or Tweeting
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + "test");
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+
+        }
+
+
+    }
+}
---
+++ Quartz/JobScheduler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz.Impl;
+
+namespace Quartz
+{
+    public class JobScheduler
+    {
+        public void Start()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<TestJob>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithSimpleSchedule(a => a.WithIntervalInMinutes(1).RepeatForever())
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+
+        public void Stop()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Shutdown();
+        }
+
+    }
+}
